a1146c014cab9488405b92f011e498b5f9e880fe
Sam Steele <sam.steele@gmail.com>
2010-11-19 22:15:48 -0800

    Allow a button to enable/disable the wakup alarm

--- wakeup.pl
+++ wakeup.pl
@@ -1,9 +1,18 @@
-if(time_now '6:59 am' and $bedroom_fan->state eq 'on') {
-	set $bedroom_fan 'off';
+my $wakeup_time = "7:00 am";
+
+if(state_now $wakeup_alarm eq 'on') {
+        speak "Wake up alarm scheduled for " . $wakeup_time;
 }
-if(time_now '7:00 am') {
-	set $bedroom_light 'on';
-	speak "Good morning, Sam.  It is now $Time_Now on $Date_Now_Speakable.";
-	speak "The outside temperature is " . round($Weather{TempOutdoor}) . " degrees.";
+
+if(state_now $wakeup_alarm eq 'off') {
+        speak "Wake up alarm is disabled";
+}
+
+if(time_now $wakeup_time and state $wakeup_alarm eq 'on') {
+        set $bedroom_light 'on';
+        set $bedroom_fan 'off';
+        speak "Good morning, Sam.  It is now $Time_Now on $Date_Now_Speakable.";
+        speak "The outside temperature is " . round($Weather{TempOutdoor}) . " degrees.";
 #TODO: Start boxee playing music
 }
+
